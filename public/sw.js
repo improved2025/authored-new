2aaf9c568e9453143b359a5ccc81b1765bce39b9
Jeff Innocent <jefinno73@gmail.com>
2026-03-02 14:08:36 -0500

    Add no-op sw.js to avoid 404 noise

---
+++ public/sw.js
@@ -0,0 +1,3 @@
+// no-op service worker placeholder
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
